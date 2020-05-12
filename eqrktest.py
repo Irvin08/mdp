e96ce68036e787f6ff0878f6331602fbb9a2d68d
Irvin08 <irvincarrillo08@gmail.com>
2020-05-12 15:31:31 -0500

    Updated where the copy is saved
    Now saves to Irvin tech folder and allows for user input of system, will be able to call this function to print details for any given system

--- eqrktest.py
+++ eqrktest.py
@@ -3,40 +3,46 @@ from openpyxl import load_workbook
 import win32com.client as win32
 
 # Only need to run this once to create a copy of eqrk status saved in a newer format.
-# After the copy is made only need to ru this to update the copy
+# After the copy is made only need to run this to update the copy
 
-##fname = r'\\amat.com\Folders\Austin\Global-Ops\AMO\CPI_TestWorkCntr\TECH FOLDERS\ Irvin Carrillo\EQRK Status.xls'
+##fname = r'\\amat.com\Folders\Austin\Global-Ops\AMO\CPI_TestWorkCntr\SUPERVISOR PASSDOWN\EQRK Status.xls'
 ##excel = win32.gencache.EnsureDispatch('Excel.Application')
 ##wb = excel.Workbooks.Open(fname)
 ##
-##wb.SaveAs(fname+"x", FileFormat = 51)    #FileFormat = 51 is for .xlsx extension
+##wb.SaveAs(r'\\amat.com\Folders\Austin\Global-Ops\AMO\CPI_TestWorkCntr\TECH FOLDERS\ Irvin Carrillo\EQRK Status.xlsx', FileFormat = 51)    #FileFormat = 51 is for .xlsx extension
 ##wb.Close()                               #FileFormat = 56 is for .xls extension
 ##excel.Application.Quit()
 ##del excel
 
-#Looks for all ready racks for given system
+#Looks for all ready racks for given system, needs updated eqrk status to work properly
+
+
 wb = load_workbook(r'\\amat.com\Folders\Austin\Global-Ops\AMO\CPI_TestWorkCntr\TECH FOLDERS\ Irvin Carrillo\EQRK Status.xlsx')
 ws = wb["Sheet1"]
+def printStatus(system):
+    for row in ws.rows:
+        if row[0].value == system:
+            for x in range(8,12):
+                if row[x].value == "NA":
+                    print("There is no EQRK #" + str(x - 7) + "\n")
+                elif row[x].value == None:
+                    print("EQRK #" + str(x - 7) + " is not ready, needed for:")
+                    x = str(row[x].comment)
+                    y = x.find('CH')
+                    z = x.find('by')
+                    print(x[y:z-1] + "\n")
+                else:
+                    print("EQRK #" + str(x - 7) + " is complete for:")
+                    #May want to get completed date: row[x].value
+                    x = str(row[x].comment)
+                    y = x.find('CH')
+                    z = x.find('by')
+                    print(x[y:z-1] + "\n")
+            try:
+                print("Comments: " + row[12].value)
+            except:
+                pass
 
-for row in ws.rows:
-    if row[0].value == "B01198":
-        for x in range(8,12):
-            if row[x].value == "NA":
-                print("There is no EQRK #" + str(x - 7) + "\n")
-            elif row[x].value == None:
-                print("EQRK #" + str(x - 7) + " is not ready, needed for:")
-                x = str(row[x].comment)
-                y = x.find('CH')
-                z = x.find('by')
-                print(x[y:z-1] + "\n")
-            else:
-                print("EQRK #" + str(x - 7) + " is complete for:")
-                #print(row[x].value)
-                x = str(row[x].comment)
-                y = x.find('CH')
-                z = x.find('by')
-                print(x[y:z-1] + "\n")
-        try:
-            print("Comments: " + row[12].value)
-        except:
-            pass
+system = input("Enter system: ")
+#maybe add a way to be able to input lowercase too
+printStatus(system)
